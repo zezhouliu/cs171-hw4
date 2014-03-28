cf1fc42a597081e1df17ac3d8a09bff36b7ef1a5
Alex Liu <zezhouliu@college.harvard.edu>
2014-03-27 21:53:41 -0400

    Finished required hw

--- ProblemGeoUSA/GeoUSA.js
+++ ProblemGeoUSA/GeoUSA.js
@@ -24,14 +24,14 @@ var bbVis = {
 var bbDetail = {
     
     x: 100,
-    y: 10,
+    y: 50,
     w: 450,
     h: 300
 }
 
 var detailVis = d3.select("#detailVis").append("svg").attr({
     width:550,
-    height:400
+    height:600
 })
 
 var canvas = d3.select("#vis").append("svg").attr({
@@ -160,7 +160,7 @@ function loadStations(c) {
         }
 
         // create scale for radius
-        var radius_scale = d3.scale.linear().domain([min_sum, max_sum]).range([2, 3.5]);
+        var radius_scale = d3.scale.linear().domain([min_sum, max_sum]).range([1.5, 6]);
 
         svg.selectAll("circle")
         .data(filtered_data)
@@ -252,43 +252,70 @@ var tooltip = d3.select("body")
 // ALL THESE FUNCTIONS are just a RECOMMENDATION !!!!
 var createDetailVis = function () {
 
-    xScale = d3.scale.linear().domain([0, 23]).range([bbDetail.x, bbDetail.w]);
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(6);
+    var midnight = new Date(2012, 0, 0, 0, 0, 0, 0);
+    var midnight1 = new Date(2012, 0, 1, 0, 0, 0, 0);
+    xScale = d3.time.scale().domain([midnight, midnight1]).range([bbDetail.x, bbDetail.w]);
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.time.format("%H:00"));;
 
     // define the scale and axis for y
     yScale = d3.scale.linear().domain([0, 10]).range([bbDetail.y, bbDetail.h]);
-    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
 
     // Add the X Axis for Overview
     detailVis.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + bbDetail.h + ")")
+        .attr("transform", "translate(0," + (bbDetail.h + bbDetail.y) + ")")
         //.style("visibility", "hidden")
         .call(xAxis);
 
     // Add the Y Axis for the Overview
     detailVis.append("g")
         .attr("class", "y axis")
-        .attr("transform", "translate(" + bbDetail.x + ",0)")
+        .attr("transform", "translate(" + bbDetail.x + "," + bbDetail.y + ")")
         //.style("visibility", "hidden")
         .call(yAxis);
 
+    detailVis.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0)
+        .attr("x", 0 - (bbDetail.h / 2) - bbDetail.y)
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Solar Radiation");
+
+    detailVis.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "translate(" + ((bbDetail.w + bbDetail.x)/ 2) + " ," + (bbDetail.h + bbDetail.y * 2) + ")")
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Hour (Military Time)");
+
+    detailVis.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "translate(" + ((bbDetail.w + bbDetail.x) / 2) + " ," + 0 + ")")
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Detailed Solar Radiation over Time");
+
+    detailVis.append("text")
+        .attr("class", "axis-label detail-uid")
+        .attr("transform", "translate(" + 0 + " ," + 50 + ")")
+        .attr("dy", "1em")
+        .style("text-anchor", "left")
+        .text("Station: None Selected");
 }
 
 
 var updateDetailVis = function (data, station_id) {
     
+    detailVis.selectAll(".detail-uid").text("Station: " + data["STATION"] + " (" + station_id + ")");
+
     if (!hourly_aggregate[station_id]) {
-        // Make invisible Y Axis for the Overview
-        detailVis.selectAll(".y")
-            .style("visibility", "hidden")
-            .call(yAxis);
+        var bars = detailVis.selectAll("rect").remove();
+        return;
     }
     else {
-        console.log(station_id);
-
-        // grab the hourly data for our station_id
-        console.log(hourly_aggregate[station_id]);
 
         var min_val = 99999999999;
         var max_val = 0;
@@ -309,6 +336,9 @@ var updateDetailVis = function (data, station_id) {
         detailVis.selectAll(".y")
             .style("visibility", "visible")
             .call(yAxis);
+        detailVis.selectAll(".x")
+            .style("visibility", "visible");
+        detailVis.selectAll(".detail-uid").text("Station: " + data["STATION"] + " (" + station_id + ")");
 
         var bars = detailVis.selectAll("rect").remove(); 
 
@@ -316,10 +346,11 @@ var updateDetailVis = function (data, station_id) {
             .data(hourly_aggregate[station_id])
             .enter()
             .append("rect")
+            .attr("class", "bar")
             .attr("x", function (d, i) {
                 return ((bbDetail.w - bbDetail.x) / 24) * i + bbDetail.x;
             })
-            .attr("y", function (d) { return yScale(d); })
+            .attr("y", function (d) { return yScale(d) + bbDetail.y; })
             .attr("width", function (d) { return (bbDetail.w - bbDetail.x) / 24; })
             .attr("height", function (d) { return bbDetail.h - yScale(d); })
             .attr("stroke", "steelblue")
